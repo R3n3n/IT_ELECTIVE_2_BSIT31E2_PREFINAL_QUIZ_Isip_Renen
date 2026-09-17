d3a402fc5922de923641433b5bb3f5649afca60e
Renen <Renen@DESKTOP-1B94TU9>
2026-09-17 19:34:15 +0800

    Added MVC Project File

---
+++ RenenPortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RenenPortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
