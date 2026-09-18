38600f8aacd022a6988d1cc6c9dd6718337d1786
Renen <Renen@DESKTOP-1B94TU9>
2026-09-18 17:02:07 +0800

    Updated Home Controller

--- RenenPortfolio/Controllers/HomeController.cs
+++ RenenPortfolio/Controllers/HomeController.cs
@@ -1,25 +1,41 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RenenPortfolio.Models;
-using System.Diagnostics;
+using RenenPortfolio.Services;
 
-namespace RenenPortfolio.Controllers
+namespace RenenPortfolio.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly IProjectService _projects;
+
+    public HomeController(IProjectService projects) => _projects = projects;
+
+    [HttpGet]
+    public IActionResult Index() => View(_projects.GetAll());
+
+    [AllowAnonymous]
+    [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
+    public IActionResult Error(int? statusCode = null)
     {
-        public IActionResult Index()
+        var model = new ErrorViewModel
         {
-            return View();
-        }
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier,
+            StatusCode = statusCode ?? 500
+        };
 
-        public IActionResult Privacy()
+        (model.Title, model.Detail) = model.StatusCode switch
         {
-            return View();
-        }
+            404 => ("No page here",
+                    "That project does not exist. Pick one from the table of contents."),
+            403 => ("Not allowed",
+                    "You are signed in, but this page is not available to you."),
+            _ => ("Something went wrong",
+                    "The page could not be loaded. Try again from the table of contents.")
+        };
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        Response.StatusCode = model.StatusCode;
+        return View(model);
     }
 }
