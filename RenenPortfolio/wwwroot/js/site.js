46c7e4c2f962422054162883cf2040a44242b9b0
Renen <Renen@DESKTOP-1B94TU9>
2026-09-18 17:12:32 +0800

    Updated site.css and site.js

--- RenenPortfolio/wwwroot/js/site.js
+++ RenenPortfolio/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.querySelectorAll('.comment-delete').forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+        if (!window.confirm('Delete this comment?')) {
+            event.preventDefault();
+        }
+    });
+});
