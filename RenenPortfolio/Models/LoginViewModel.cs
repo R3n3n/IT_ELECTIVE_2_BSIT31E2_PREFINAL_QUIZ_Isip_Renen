0a4288464a0fa0ebf23a1c9e6a2ae5ff405627f1
Renen <Renen@DESKTOP-1B94TU9>
2026-09-18 16:52:16 +0800

    Added Login View Model

---
+++ RenenPortfolio/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RenenPortfolio.Models;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Enter your username.")]
+    [StringLength(64)]
+    [Display(Name = "Username")]
+    public string Username { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Enter your password.")]
+    [DataType(DataType.Password)]
+    [StringLength(128)]
+    [Display(Name = "Password")]
+    public string Password { get; set; } = string.Empty;
+
+    [Display(Name = "Keep me signed in")]
+    public bool RememberMe { get; set; }
+    public string? ReturnUrl { get; set; }
+}
