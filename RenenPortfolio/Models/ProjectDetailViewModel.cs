9d94e382a56ca4f5d94ab3a554cb3a8792ca3ab6
Renen <Renen@DESKTOP-1B94TU9>
2026-09-18 16:53:10 +0800

    Added Project Detail View Model

---
+++ RenenPortfolio/Models/ProjectDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RenenPortfolio.Models;
+
+public class ProjectDetailViewModel
+{
+    public Project Project { get; set; } = new();
+    public IReadOnlyList<Comment> Comments { get; set; } = Array.Empty<Comment>();
+    public Comment NewComment { get; set; } = new();
+    public Project? Previous { get; set; }
+    public Project? Next { get; set; }
+    public IReadOnlyList<Project> AllProjects { get; set; } = Array.Empty<Project>();
+    public IReadOnlyDictionary<string, int> CommentCounts { get; set; } = new Dictionary<string, int>();
+}
