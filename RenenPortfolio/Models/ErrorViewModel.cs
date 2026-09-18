cd35292d95482323777f2a46b0d9629d3d658e92
Renen <Renen@DESKTOP-1B94TU9>
2026-09-18 16:54:52 +0800

    Updated Error View Model

--- RenenPortfolio/Models/ErrorViewModel.cs
+++ RenenPortfolio/Models/ErrorViewModel.cs
@@ -1,9 +1,10 @@
-namespace RenenPortfolio.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace RenenPortfolio.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    public int StatusCode { get; set; } = 500;
+    public string Title { get; set; } = "Something went wrong";
+    public string Detail { get; set; } = "The page could not be loaded. Try again from the table of contents.";
 }
